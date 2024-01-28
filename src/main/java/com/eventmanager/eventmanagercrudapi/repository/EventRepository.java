4e964be58ac2daa373db335365a14beacfd36b54
ori-frenkel <49747014+ori-frenkel@users.noreply.github.com>
2024-01-28 22:45:23 +0200

    adding sort and get by location
    adding two features:
    # Retrieve events based on location
    # * Sort events by date, popularity (number of participants), or creation time

--- src/main/java/com/eventmanager/eventmanagercrudapi/repository/EventRepository.java
+++ src/main/java/com/eventmanager/eventmanagercrudapi/repository/EventRepository.java
@@ -1,10 +1,18 @@
 package com.eventmanager.eventmanagercrudapi.repository;
 
 import com.eventmanager.eventmanagercrudapi.model.Event;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findById(int id);
+    List<Event> findByLocation(String location);
+    List<Event> findByLocation(String location, Sort sort);
+
+    List<Event> findAll(Sort sort);
 }
--- src/main/java/com/eventmanager/eventmanagercrudapi/service/EventService.java
+++ src/main/java/com/eventmanager/eventmanagercrudapi/service/EventService.java
@@ -4,6 +4,7 @@ import com.eventmanager.eventmanagercrudapi.exceptions.EventNotFoundException;
 import com.eventmanager.eventmanagercrudapi.model.Event;
 import com.eventmanager.eventmanagercrudapi.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +21,26 @@ public class EventService {
     public void addEvent(Event event) {
         eventRepository.save(event);
     }
-    public List<Event> getAllEvents() {
-        return eventRepository.findAll();
+    public List<Event> getAllEvents(String location,String sortBy, String sortDirection) {
+        List<Event> events;
+        boolean isLocationEmpty = location == null || location.isEmpty();
+        boolean isSortByEmpty = sortDirection == null || sortDirection.isEmpty();
+
+        if (!isLocationEmpty && !isSortByEmpty) {
+            // find by location and by given sort
+            events = eventRepository.findByLocation(location, getSortBy(sortBy, sortDirection));
+        } else if (!isLocationEmpty) {
+            // find by location only
+            events = eventRepository.findByLocation(location);
+        } else if (isSortByEmpty){
+            // find by given sort only
+            events = eventRepository.findAll(getSortBy(sortBy, sortDirection));
+        } else {
+            // find all without specific location or sort
+            events = eventRepository.findAll();
+        }
+
+        return events;
     }
 
     public Event getEventById(Long id) {
@@ -55,10 +74,35 @@ public class EventService {
         if (optionalEvent.isPresent()) {
             Event eventToDelete = optionalEvent.get();
             eventRepository.delete(eventToDelete);
+
             return true;
         } else {
             throw new EventNotFoundException(eventId);
         }
     }
 
+    private Sort.Direction getSortDirection(String sortDirection) {
+        if (sortDirection == null || sortDirection.isEmpty()) {
+            return Sort.Direction.ASC;
+        }
+
+        if (sortDirection.equalsIgnoreCase("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+    }
+
+    private Sort getSortBy(String sortBy, String sortDirection) {
+        if (sortBy == null || sortBy.isEmpty()) {
+            return Sort.by(Sort.Direction.ASC, "date");
+        }
+
+        Sort.Direction direction = getSortDirection(sortDirection);
+        return switch (sortBy.toLowerCase()) {
+            case "popularity" -> Sort.by(direction, "popularity");
+            case "creationtime" -> Sort.by(direction, "creationTime");
+            default -> Sort.by(direction, "date");
+        };
+    }
 }
